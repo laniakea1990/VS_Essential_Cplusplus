62969f7a7ad7afd33cec57e648a6d021eea1bfbf
raiseking <jinranran1990910@163.com>
2015-04-04 17:16:10 +0800

    0404@2

---
+++ Exercise_Chapter4/stack_test.cpp
@@ -0,0 +1,39 @@
+#include "Stack.h"
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	Stack st;
+	string str;
+	
+	while (cin >> str && !st.full())
+	{
+		st.push(str);
+	}
+	
+	if (st.empty())
+	{
+		cout << '\n' << "Oops: no string were read  -- bailing out\n";
+		return 0;
+	}
+	st.peek(str);
+
+	if (st.size() == 1 && str.empty())
+	{
+		cout << '\n' << "Oops: no string were read -- bailing out\n";
+		return 0;
+	}
+
+	cout << '\n' << "Read in " << st.size() << "strings!\n"
+		<< "The strings, in reverser order: \n";
+	while (st.size())
+		if (st.pop(str))
+			cout << str << ' ';
+	cout << '\n' << "There are now " << st.size()
+		<< " elements in the stack!\n";
+	getchar();
+}
